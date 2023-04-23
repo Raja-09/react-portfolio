a9c1d15ec08a39d83f76afd6d76bd5eaa96466cb
Raja <aravindha92@gmail.com>
2023-04-23 20:11:40 +0530

    Add pacman loader

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCss3, faGitAlt, faHtml5, faJava, faJs, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 const About = () => {
     const [animateClass, setAnimateClass] = useState("text-animate");
@@ -12,43 +13,46 @@ const About = () => {
         }, 3000);
     }, [])
     return (
-        <div className="container about-page">
-            <div className="text-zone">
-                <h1>
-                    <AnimatedLetters letterClass={animateClass} strArray={"About me".split("")}
-                        idx={15} />
-                </h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, eius accusantium sapiente necessitatibus alias possimus iure hic optio deserunt consequuntur earum commodi voluptates doloremque laborum, iusto expedita excepturi perspiciatis quis.</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur vitae consequatur vero natus cumque necessitatibus unde ullam voluptas magnam corrupti voluptate nisi voluptatum, quia, maxime tenetur, libero voluptatem nobis adipisci.</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos sint porro maxime, temporibus saepe hic illo quisquam numquam quia? Saepe inventore veritatis in nisi nesciunt praesentium! Tenetur, accusamus similique ad non, perspiciatis voluptatum totam sit quae voluptates, incidunt obcaecati facilis facere numquam soluta temporibus voluptas excepturi deserunt. Fugiat, unde modi?</p>
+        <>
+            <div className="container about-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters letterClass={animateClass} strArray={"About me".split("")}
+                            idx={15} />
+                    </h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, eius accusantium sapiente necessitatibus alias possimus iure hic optio deserunt consequuntur earum commodi voluptates doloremque laborum, iusto expedita excepturi perspiciatis quis.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur vitae consequatur vero natus cumque necessitatibus unde ullam voluptas magnam corrupti voluptate nisi voluptatum, quia, maxime tenetur, libero voluptatem nobis adipisci.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos sint porro maxime, temporibus saepe hic illo quisquam numquam quia? Saepe inventore veritatis in nisi nesciunt praesentium! Tenetur, accusamus similique ad non, perspiciatis voluptatum totam sit quae voluptates, incidunt obcaecati facilis facere numquam soluta temporibus voluptas excepturi deserunt. Fugiat, unde modi?</p>
 
 
-            </div>
-
-            <div className="stage-cube-cont">
-                <div className="cube-spinner">
-                    <div className="face1">
-                        <FontAwesomeIcon icon={faReact} color='#5ed4f4' />
-                    </div>
-                    <div className="face2">
-                        <FontAwesomeIcon icon={faHtml5} color='#f06529' />
-                    </div>
-                    <div className="face3">
-                        <FontAwesomeIcon icon={faCss3} color='#28a4d9' />
-                    </div>
-                    <div className="face4">
-                        <FontAwesomeIcon icon={faJsSquare} color='#efd81d' />
-                    </div>
-                    <div className="face5">
-                        <FontAwesomeIcon icon={faGitAlt} color='#ec4d28' />
-                    </div>
-                    <div className="face6">
-                        <FontAwesomeIcon icon={faNodeJs} color='green'/>
-                    </div>
                 </div>
 
+                <div className="stage-cube-cont">
+                    <div className="cube-spinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faReact} color='#5ed4f4' />
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faHtml5} color='#f06529' />
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faCss3} color='#28a4d9' />
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faJsSquare} color='#efd81d' />
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faGitAlt} color='#ec4d28' />
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faNodeJs} color='green' />
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 }
 export default About;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from '../Logo';
+import Loader from 'react-loaders';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -15,25 +16,21 @@ const Home = () => {
             return setLetterClass('text-animate-hover');
         }, 4500);
     }, [])
-    return (
+    return (<>
+
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
                     <span className={letterClass}>H</span>
                     <span className={`${letterClass} _12`}>i</span>
-
                     <br />
-
                     <span className={`${letterClass} _13`}>I</span>
                     <span className={`${letterClass} _14`}>'m</span>
-
                     <img src={LogoTitle} alt="R" />
-
                     <AnimatedLetters letterClass={letterClass}
                         strArray={nameArray}
                         idx={15} />
                     <br />
-
                     <AnimatedLetters letterClass={letterClass}
                         strArray={jobArray}
                         idx={28}
@@ -44,6 +41,8 @@ const Home = () => {
             </div>
             <Logo />
         </div>
+        <Loader type="pacman" />
+    </>
     );
 };
 
